0242cb927181604d9416efcedac604fd4101f8a3
erikmaulanahardi <99226753+erikmaulanahardi@users.noreply.github.com>
2023-09-24 03:05:28 +0700

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,16 @@
+//Aktivasi Toggle
+const navbarNav = document.querySelector(".navbar-nav");
+
+//On Click
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Hiding 
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
